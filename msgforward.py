b38b6d0e84c7f2f82a6def94713281026c1c3a64
zhangjiangpo <962931566@qq.com>
2017-01-11 19:58:14 +0800

    tj

--- msgforward.py
+++ msgforward.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 from wxbot import *
+import os
 
 class msgForward(WXBot):
 	def __init__(self):
@@ -8,13 +9,26 @@ class msgForward(WXBot):
 
 	def handle_msg_all(self, msg):
 
-		if msg['msg_type_id'] == 4 and msg['content']['type'] == 0: #user send msg
-			if not self.send_msg_by_uid(msg['content']['data'],self.get_user_id('龙尔尔')):
+		username = self.get_user_id('龙尔尔');
+
+		if msg['msg_type_id'] == 37 and msg['content']['type'] == 37: #user add request
+			
+			if not self.apply_useradd_requests(msg['content']['data']):
+				print 'add friend error'
+
+		if msg['msg_type_id'] == 4 and msg['content']['type'] == 0 : #user send msg
+			if not self.send_msg_by_uid(msg['content']['data'],username):
+				print 'send msg error'
+
+		if msg['msg_type_id'] == 4 and msg['content']['type'] == 3 : #user send img
+			imgpath = self.get_msg_img(msg['msg_id'])
+			print imgpath
+			if not self.send_img_msg_by_uid(os.path.join(self.temp_pwd,imgpath),username):
 				print 'send msg error'
 
 def main():
     bot = msgForward()
-    bot.DEBUG = True
+    #bot.DEBUG = True
     bot.conf['qr'] = 'png'
     bot.is_big_contact = False
     bot.run()
